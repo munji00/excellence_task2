eac830ffe13a70644d201cdc7ed7002b8d5b7cda
mohd umair siddiqui <mohdumairsiddiqui00@gmail.com>
2023-11-15 18:06:13 +0530

    task 4 completed

--- routes/ecomProdRoutes.js
+++ routes/ecomProdRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { fetchMobileProducts } from '../controllers/productControllers.js';
+import { fetchMobileProducts, fetch_t_shirt } from '../controllers/productControllers.js';
 
 const prodRoute = express.Router();
 
 prodRoute.get('/flipkart/mobile' , fetchMobileProducts);
+prodRoute.get('/snapdeal/t-shirt', fetch_t_shirt);
 
 export default prodRoute
